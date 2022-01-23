5b684d17442a43467d2a59296737964d1e54f9ff
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-01-23 13:20:58 +0100

    Сделал дз №2 в данном блоке

--- src/store/todoAction.js
+++ src/store/todoAction.js
@@ -15,3 +15,13 @@ export const removeTodoTask = (id) => ({
     type: "REMOVE_TASK",
     payload: id,
 });
+
+
+export const setCheckedTodoTasks= (checked) => ({
+    type: "SET_CHECKED_TASKS",
+    payload: checked
+});
+
+export const clearTodoList = () => ({
+    type: "CLEAR_TODO_LIST"
+});
