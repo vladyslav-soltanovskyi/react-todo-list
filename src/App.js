cf93e134eaeaf5fab7bff3db903b8587839b578f
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-01-29 20:40:31 +0100

    исправил баг с юзэфектом

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
     useEffect(() => {
         dispatch(fetchTasks());
-    }, [dispatch]);
+    }, [dispatch]);  
 
     const editTask = (id, data) => {
         dispatch(editTodoTask(id, data));
