a6560021f93a966db6bf4ced71024da45713b007
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-01-29 20:57:59 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ function App() {
     
     let isAllTasksCompleted = tasks.every(task => task.completed);
 
+    
     useEffect(() => {
         dispatch(fetchTasks());
-    }, [dispatch]);
+    }, [dispatch]);  
 
     const editTask = (id, data) => {
         dispatch(editTodoTask(id, data));
